69f8ad858dcfdb0bd090085a0c6f8310ca655430
tatarize <tatarize@gmail.com>
2018-12-26 20:43:00 -0800

    +PLTE write blocks, couple hooks.

--- PNGRaster.py
+++ PNGRaster.py
@@ -16,18 +16,34 @@ class PngRaster:
     http://www.libpng.org/pub/png/spec/1.2/PNG-Chunks.html
     """
 
-    def __init__(self, width, height, bit_depth=8, color_type=2):
-        self.color_type = color_type
-        self.bit_depth = bit_depth
-        self.samples_per_pixel = 1
-        self.width = width
-        self.height = height
-        self.samples_per_pixel = self.get_sample_count(color_type)
-        self.stride = self.get_stride(self.samples_per_pixel, self.bit_depth, self.width)
-        self.buf = []
-        for i in range(height):
-            line = bytearray(b'\x00' + b'\xFF' * self.stride)
-            self.buf.append(line)
+    def __init__(self, width=None, height=None, bit_depth=8, color_type=2):
+        if height is not None:
+            self.width = width
+            self.height = height
+            self.bit_depth = bit_depth
+            self.color_type = color_type
+            self.samples_per_pixel = self.get_sample_count(color_type)
+            self.stride = self.get_stride(self.samples_per_pixel, self.bit_depth, self.width) + 1
+            self.palette = None
+            self.buf = []
+            for i in range(height):
+                line = bytearray(b'\x00' * self.stride)
+                self.buf.append(line)
+
+    def index_color(self, index, value=None):
+        byte_index_start = index * 3
+        byte_index_end = byte_index_start + 3
+        if self.palette is None:
+            self.palette = bytearray(b'')
+        if byte_index_end > len(self.palette):
+            self.palette += b'\x00' * (byte_index_end - len(self.palette))
+        section = self.palette[byte_index_start: byte_index_end]
+        color = int.from_bytes(section, byteorder='big', signed=False)
+        if value is not None:
+            for pos in range(byte_index_end - 1, byte_index_start - 1, -1):
+                self.palette[pos] = value & 0xff
+                value >>= 8
+        return color
 
     def pixel(self, x, y, sample=None):
         """
@@ -55,8 +71,11 @@ class PngRaster:
             value &= ~(mask_sample_bits << unused_bits_right_of_sample)
             value |= (sample & mask_sample_bits) << unused_bits_right_of_sample
             for pos in range(end_pos_in_bytes, start_pos_in_bytes - 1, -1):
-                scanline[pos] = value & 0xff
-                value >>= 8
+                try:
+                    scanline[pos] = value & 0xff
+                    value >>= 8
+                except IndexError:
+                    print(pos, len(scanline))
         return original
 
     @staticmethod
@@ -92,9 +111,14 @@ class PngRaster:
             chunk_head = png_tag + data
             return struct.pack("!I", len(data)) + chunk_head + struct.pack("!I", 0xFFFFFFFF & zlib.crc32(chunk_head))
 
+        if self.palette is None:
+            plte = b''
+        else:
+            plte = png_pack(b'PLTE', self.palette)
         return b"".join([
             b'\x89PNG\r\n\x1a\n',
             png_pack(b'IHDR', struct.pack("!2I5B", width, height, self.bit_depth, self.color_type, 0, 0, 0)),
+            plte,
             png_pack(b'IDAT', zlib.compress(raw_data, 9)),
             png_pack(b'IEND', b'')])
 
@@ -131,13 +155,15 @@ class PngRaster:
                 self.height = struct.unpack(">I", data[4:8])[0]
                 self.bit_depth = data[8]
                 self.color_type = data[9]
+            elif signature == 'PLTE':
+                self.palette = bytearray(data)
             elif signature == 'IDAT':
                 zlib_data += data
             elif signature == 'IEND':
                 break
         png_data = zlib.decompress(zlib_data)
-        self.stride = self.get_stride(self.samples_per_pixel, self.bit_depth, self.width)
         self.samples_per_pixel = self.get_sample_count(self.color_type)
+        self.stride = self.get_stride(self.samples_per_pixel, self.bit_depth, self.width) + 1
         self.buf = [
             bytearray(png_data[line:line + self.stride])
             for line in range(0, len(png_data), self.stride)
@@ -192,6 +218,21 @@ class PngRaster:
                 fraction += dx
                 self.plot(x0, y0, color)
 
+    def fill(self, color):
+        for y in range(self.height):
+            for x in range(self.width):
+                self.pixel(x, y, color)
+
     def plot(self, x, y, color):
         if 0 <= x < self.width and 0 <= y < self.height:
             self.pixel(x, y, color)
+
+
+if __name__ == "__main__":
+    raster = PngRaster(100, 100, 1, 3)
+    raster.fill(0)
+    raster.draw_line(0, 0, 100, 100, 1)
+    raster.draw_line(0, 100, 100, 0, 1)
+    raster.index_color(0, 0x00FF00)
+    raster.index_color(1, 0xFF0000)
+    raster.save_png("default.png")
